d1bbfe3f52a1a231c337b6b0d062c894e365a5f7
Gayorgii <george_koval@mail.ru>
2025-10-14 22:28:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('changeTextButton');
     button.addEventListener('click', function() {
         if (but ==0){
-        document.querySelector('h1').textContent = 'ХРЮЮЮЮ';
+        document.querySelector('h1').textContent = 'Хрю';
             but = 1;
         }else{
             document.querySelector('h1').textContent = 'Жми на пятачок';
+            but = 0;
         }
     });
 });
+
 
 
 
